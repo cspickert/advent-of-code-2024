333c161ef5fd434d1db0dbe002966bb21e8b6108
Cameron Spickert <cspickert@users.noreply.github.com>
2024-12-20 15:20:23 -0500

    Day 5 part 1 and 2

---
+++ day05.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from collections import defaultdict
+from functools import cmp_to_key
+
+
+def part1(data):
+    rules, updates = data
+    result = 0
+    for update in updates:
+        if validate_update(rules, update):
+            result += get_middle_value(update)
+    return result
+
+
+def part2(data):
+    rules, updates = data
+    result = 0
+    for update in updates:
+        if not validate_update(rules, update):
+            fixed_update = get_fixed_update(rules, update)
+            result += get_middle_value(fixed_update)
+    return result
+
+
+def validate_update(rules, update):
+    for before, after in zip(update, update[1:]):
+        if before in rules[after]:
+            return False
+    return True
+
+
+def get_fixed_update(rules, update):
+    def comparator(before, after):
+        if before in rules[after]:
+            return 1
+        return -1
+
+    return sorted(update, key=cmp_to_key(comparator))
+
+
+def get_middle_value(values):
+    return values[len(values) // 2]
+
+
+def parse_data(input_file):
+    rules_str, updates_str = input_file.read().split("\n\n")
+
+    rules_lines = rules_str.split()
+    rules_list = [rule_line.split("|") for rule_line in rules_lines]
+    rules = defaultdict(set)
+    for before, after in rules_list:
+        rules[int(before)].add(int(after))
+
+    updates_lines = updates_str.split()
+    updates_list = [update_line.split(",") for update_line in updates_lines]
+    updates = [[int(value) for value in update] for update in updates_list]
+
+    return rules, updates
+
+
+if __name__ == "__main__":
+    input_file = (Path(__file__).parent / "input" / "day05.txt").open()
+    data = parse_data(input_file)
+    print(part1(data))
+    print(part2(data))
---
+++ tests/test_day05.py
@@ -0,0 +1,47 @@
+import pytest
+from io import StringIO
+from day05 import parse_data, part1, part2
+
+
+@pytest.fixture
+def example_input():
+    return StringIO("""\
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+""")
+
+
+def test_part1(example_input):
+    data = parse_data(example_input)
+    assert part1(data) == 143
+
+
+def test_part2(example_input):
+    data = parse_data(example_input)
+    assert part2(data) == 123
